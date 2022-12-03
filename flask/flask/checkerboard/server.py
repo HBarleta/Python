3c45c8cbe93844dc2800f8ef42d1e4d63e9ab34b
Harold Barleta <hbf20c@gmail.com>
2022-12-02 23:58:11 -0800

    fixed server spacing

--- flask/flask/checkerboard/server.py
+++ flask/flask/checkerboard/server.py
@@ -7,9 +7,5 @@ def checkerboard(row, column, color1, color2):
     return render_template('index.html', x=row, y=column, color1=color1, color2=color2)
 
 
-
-
-
-
 if __name__ == "__main__":
     app.run(debug=True, port=5001,host="0.0.0.0")
