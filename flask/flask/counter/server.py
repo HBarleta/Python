5705077c719b7ff7c3d97efa7de3ece27b121201
Harold Barleta <hbf20c@gmail.com>
2022-12-04 22:27:25 -0800

    counter assignment completed

--- flask/flask/counter/server.py
+++ flask/flask/counter/server.py
@@ -2,25 +2,33 @@ from flask import Flask, render_template, session, redirect, request
 
 app = Flask(__name__)
 app.secret_key = "secrets are for losers"
-
+visits = 0
+# global variable is accessible inside this entire program
 
 @app.route('/')
 def home():
-    counter = 0
+    # global variable used to keep track of page visits
+    global visits
+    # this conditional will check if a session['click'] is inside session dict. This means a click was already performed on button
     if 'click' in session:
-        counter += 1
-        print('counter')
-    return render_template("index.html", counter=counter)
+        visits += 1
+    # count = visits in order to link this global variable to the HTML jinja variable
+    count = visits 
+    return render_template("index.html", count = count )
 
 @app.route('/count', methods = ['POST'])
-def counter():
+def counter():    
     session['click'] = request.form
-    print(session)
+    # request.form store in a session cookie to keep track of IF the click button was clicked
+    print(session['click'])
     return redirect('/')
 
-@app.route('/destroy_cookies')
+@app.route('/destroy_session')
 def destroy():
+    global visits
+    visits = 0
     session.clear()
+    # sets visits back to 0 and clears session dict then redirects back to root
     return redirect('/')
 
 if __name__=="__main__":
