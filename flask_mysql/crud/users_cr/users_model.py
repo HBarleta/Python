2f70bba6e1983e885f421788842668bcb998434f
Harold Barleta <hbf20c@gmail.com>
2022-12-09 12:25:47 -0800

    usersCRUD complete

--- flask_mysql/crud/users_cr/users_model.py
+++ flask_mysql/crud/users_cr/users_model.py
@@ -1,7 +1,8 @@
 from mysqlconnection import connectToMySQL
-
+DATABASE = "users_schema"
 class Users:
     def __init__(self, data):
+        self.user_id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
         self.email = data['email']
@@ -14,18 +15,54 @@ class Users:
             SELECT * FROM users;
         """
         results = connectToMySQL("users_schema").query_db(query)
-        
+        # this will query the database and return a list of dictionaries
         all_users = []
-        
+        # this list empty list will populate with this for loop
         for user_row in results:
             this_user_instance = cls(user_row)
-            all_users.append(this_user_instance)
+            all_users.append(this_user_instance)    
+        # this for loop takes a user instance
         return all_users
     
     @classmethod
     def create(cls, data):
         query = """
-            INSERT INTO users (id, first_name, last_name, email)
-            VALUES (%(id)s,%(first_name)s,%(last_name)s,%(email)s);
+            INSERT INTO users (first_name, last_name, email)
+            VALUES (%(first_name)s,%(last_name)s,%(email)s);
         """
-        return connectToMySQL("users_schema").query_db(query, data)
+        return connectToMySQL(DATABASE).query_db(query, data)
+    
+    @classmethod
+    def show_last(cls):
+        query = """
+            SELECT * FROM users ORDER BY id DESC limit 1;
+        """
+        results = connectToMySQL(DATABASE).query_db(query)
+        this_user = cls(results[0])
+        return this_user
+        
+    @classmethod
+    def get_one_user(cls, data):
+        query = """
+            SELECT * FROM users WHERE users.id = %(id)s;
+        """
+        results = connectToMySQL(DATABASE).query_db(query, data)
+        if results:
+            return cls(results[0])
+        return False
+    
+    @classmethod
+    def update_user(cls, data):
+        query = """
+        UPDATE users
+        SET first_name = %(first_name)s, last_name = %(last_name)s , email = %(email)s
+        WHERE users.id = %(id)s;
+        """
+        return connectToMySQL(DATABASE).query_db(query, data)
+    
+    @classmethod
+    def delete_user(cls, data):
+        query = """
+            DELETE FROM users WHERE users.id = %(id)s
+        """
+        return connectToMySQL(DATABASE).query_db(query, data)
